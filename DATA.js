4f113127a135707fa011c8f2a0c230f464f1db89
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 13:20:28 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "99f96318-731e-4097-a664-bf9750b89de9",
+    "home_feed": [
+        {
+            "id": "cb74f3c4-acc9-41b3-a77d-c29ef1a0dd2c",
+            "artist_name": "worker",
+            "home_carousel": [
+                {
+                    "id": "ad5f9233-26be-41a5-89f9-9ac4b67de682",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "ca6716e2-1f9d-4a92-9181-b4bb5e899a25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "c0ac4249-3bb2-4e18-8252-3baf7bef55b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                },
+                {
+                    "id": "be1d01a7-bb14-4526-9f1e-058a2cb7048b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                },
+                {
+                    "id": "f9e135b7-37ba-4477-af05-492c558ef2a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                }
+            ],
+            "art_caption": "yes",
+            "art_description": "Later hospital consider able newspaper coach. Against interview bad mean really us. Leg trial yes far weight catch."
+        },
+        {
+            "id": "19535b8c-62b4-468b-b113-2dd50bb9513d",
+            "artist_name": "especially",
+            "home_carousel": [
+                {
+                    "id": "8bea2bb8-7bc6-4083-9c01-f61114022c4a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "0a0fe044-5bb8-4510-a36f-a73b6102d9d7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "510e8a5d-f107-4422-978c-5a76be0c831d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "33b1e081-59d9-4a89-b76b-75610f955bc8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "c8cf0e09-2c87-4b39-b6d6-27fae1799254",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                }
+            ],
+            "art_caption": "mission",
+            "art_description": "Direction cover hear reveal true. Teacher off up. Magazine not per save face his."
+        },
+        {
+            "id": "5c694c7f-1c90-4c06-a903-4182cc3ab84c",
+            "artist_name": "health",
+            "home_carousel": [
+                {
+                    "id": "506d15bd-0e82-4fce-b0c4-e2c252ee8eaf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "72c16ca9-5fe1-4cb6-b7e9-0ebb6573fb79",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                },
+                {
+                    "id": "558d8658-5fb7-42dd-b35b-9d484333139a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "d687bed9-42a7-4802-bd04-171eb9cf8f8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "ad362a44-3d81-47c2-866b-97fceb4651e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                }
+            ],
+            "art_caption": "reason",
+            "art_description": "Long travel only. Glass office seek state."
+        },
+        {
+            "id": "57ecdb88-b4c4-416c-8b77-4464f9d0dd70",
+            "artist_name": "listen",
+            "home_carousel": [
+                {
+                    "id": "0c480e20-0644-4c88-be18-94d1bb0810a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "fbce12e3-14f1-469a-bf90-1bf4086c7b89",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "9805fd13-40b9-4a9f-ad38-aebd0175ca20",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "b3b5e4d0-3b4a-43ca-8a23-df775d63fbec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png"
+                },
+                {
+                    "id": "f42493f7-2801-4389-a163-68dff7a3fb8f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                }
+            ],
+            "art_caption": "firm",
+            "art_description": "Realize TV some manage. Life figure amount him whether."
+        },
+        {
+            "id": "ba59b190-1f81-4886-9d3b-1e98261c5558",
+            "artist_name": "health",
+            "home_carousel": [
+                {
+                    "id": "2b33f924-2600-438f-a255-a7fc1d588606",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                },
+                {
+                    "id": "4d2995fd-19fa-4dab-9264-39a35bb87911",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "36374fb7-fe50-4144-9bfa-ee4047293879",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "d5928270-9b38-42d7-9e8b-25e10bf05a50",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "2ce7f3e6-4a0d-467f-b3d5-1d24811a4afe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                }
+            ],
+            "art_caption": "good",
+            "art_description": "Oil career quite compare officer see write. Produce marriage themselves industry."
+        }
+    ]
+}
